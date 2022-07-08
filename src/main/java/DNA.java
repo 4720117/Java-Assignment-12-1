e13fc1843412311fdf73a748ff76b626c796cb82
4720117 <103705616+4720117@users.noreply.github.com>
2022-07-08 10:41:30 +0900

    Update DNA.java

--- src/main/java/DNA.java
+++ src/main/java/DNA.java
@@ -1,3 +1,22 @@
+
+/**
+ * 演習問題
+ * 
+ * @author (4720117 佐藤誠弥)
+ * @version (2022/07/08)
+ */
 public class DNA {
+    public String str = "";
+    
+    public String getPair(){
+        String r="";
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i)=='A') r+="T";
+            else if(str.charAt(i)=='T') r+="A";
+            else if(str.charAt(i)=='C') r+="G";
+            else if(str.charAt(i)=='G') r+="C";
+        }
+        return r;
+    }
 
 }
